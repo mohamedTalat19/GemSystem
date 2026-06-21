adf453fa98b409e94e0859a7d97ffca3d921d1e7
mohamedTalat19 <mtalat921@gmail.com>
2026-06-21 13:59:05 +0300

    Add project files.

---
+++ GemSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GemSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
