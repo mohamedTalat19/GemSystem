b8ec4363554ac689c0478a27edcfe15ea07ad7dd
mohamedTalat19 <mtalat921@gmail.com>
2026-06-21 22:24:18 +0300

    Asseignment 1

---
+++ GemSystem/AppDbContexts/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GemSystem.Configurations;
+using GemSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GemSystem.AppDbContexts
+{
+    public class GymDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=GymManagment;Trusted_Connection=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<Plan> Plans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PlanConfiguration());
+        }
+    }
+}
---
+++ GemSystem/Controllers/PlansController.cs
@@ -0,0 +1,35 @@
+﻿using GemSystem.AppDbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GemSystem.Controllers
+{
+    public class PlansController : Controller
+    {
+
+        private readonly GymDbContext _dbContext;
+        public PlansController()
+        {
+            _dbContext = new GymDbContext();
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var plans = await _dbContext.Plans.ToListAsync();
+
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await _dbContext.Plans.FindAsync(id);
+
+            if (plan == null)
+            {
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(plan);
+        }
+    }
+}
